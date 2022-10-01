7749a36e3fa3c7f965c56b9425267ff42896c081
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-10-01 14:28:20 +0400

    added more otters

--- src/commands/otter.ts
+++ src/commands/otter.ts
@@ -20,8 +20,14 @@ export class UserCommand extends Command {
 			"https://cdn.discordapp.com/attachments/1009420908747837451/1025708051979841607/otter10.png",
 			"https://cdn.discordapp.com/attachments/1009420908747837451/1025708052395073607/otter11.png",
 			"https://cdn.discordapp.com/attachments/1009420908747837451/1025712736098865154/otter12.png",
-            "https://cdn.discordapp.com/attachments/1009420908747837451/1025712736098865154/otter12.png",
 			"https://cdn.discordapp.com/attachments/1009420908747837451/1025708052663521290/otter13.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714325299671120/otter14.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714325958164520/otter15.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714326356631552/otter16.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714326713151538/otter17.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714327048683600/otter18.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714372942778449/otter19.png",
+			"https://cdn.discordapp.com/attachments/1025714225726902302/1025714373320253530/otter20.png",
 		];
 		const num = Math.floor(Math.random() * images.length);
 		return interaction.reply({ content: `${images[num]}` });
