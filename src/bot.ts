6c4a90e4b1f4ddccad94f5a05938337af91a3c71
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-10-01 14:39:35 +0400

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -7,5 +7,5 @@ const client = new Client({
 // Load all the commands and message component handlers:
 await client.load();
 
-// Start up the HTTP server;
+// Starts up the HTTP server;
 await client.listen({ port: 3000 });
